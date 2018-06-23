96c70476338b5f0ae1109a87afafdd3cf3f2dc98
Rakesh <dkredhu@gmail.com>
2018-06-24 02:01:05 +0530

    Flappy Bird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+    public float upForce = 200f;
+    public bool isdead = false;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isdead) { return; }
+        if(Input.GetMouseButtonDown(0)){
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0, upForce));
+            anim.SetTrigger("Flap");
+
+        }
+    }
+    private void OnCollisionEnter2D()
+    {
+        isdead = true;
+        rb2d.velocity = Vector2.zero;
+        anim.SetTrigger("Die");
+        GameControl.Instance.Die();
+    }
+}
---
+++ Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour
+{
+
+    public float spawnRate = 4f;
+    public int columnPoolSize = 5;
+    public float columnYMin = -2f;
+    public float columnYMax = 2f;
+
+    public float timeSinceLastSpawn;
+    private float spawnXpos = 10f;
+    private int currentColumn = 0;
+
+    public GameObject columnsPrefab;
+
+    private GameObject[] columns;
+
+    private Vector2 objectPoolPostion = new Vector2(-15, -25f);
+	// Use this for initialization
+	void Start () {
+
+        columns = new GameObject[columnPoolSize];
+        for (int i=0; i < columnPoolSize;i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnsPrefab, objectPoolPostion, Quaternion.identity);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSinceLastSpawn += Time.deltaTime;
+        if(!GameControl.Instance.isGameOver && timeSinceLastSpawn >= spawnRate){
+            timeSinceLastSpawn = 0;
+
+            float spawnYpos = Random.Range(columnYMin, columnYMax);
+            columns[currentColumn].transform.position = new Vector2(spawnXpos, spawnYpos);
+            currentColumn++;
+            if(currentColumn >= columnPoolSize){
+                currentColumn = 0;
+
+            }
+
+        }
+	}
+}
